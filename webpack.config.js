278e0fc0f4a218f9ceb246973b559975b86593ed
Jordy Moos <jordymoos@gmail.com>
2017-12-07 20:48:54 +0100

    Added babel because we need babel you know

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,20 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env'],
+          },
+        },
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.ejs',
