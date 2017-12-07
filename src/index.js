7e2fb554b677a70358673f3c51c192cb3b648409
Jordy Moos <jordymoos@gmail.com>
2017-12-07 21:02:46 +0100

    Adding a polymer web component

---
+++ src/index.js
@@ -0,0 +1,1 @@
+import '../bower_components/paper-input/paper-input.html';
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+  },
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'node_modules'),
+      path.resolve(__dirname, 'bower_components'),
+    ],
   },
   module: {
     rules: [
@@ -19,11 +26,22 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.html$/,
+        use: [
+          'babel-loader',
+          'polymer-webpack-loader',
+        ],
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.ejs',
     }),
-  ]
+    new CopyWebpackPlugin([{
+      from: path.resolve(__dirname, 'bower_components/webcomponentsjs/*.js'),
+      to: 'bower_components/webcomponentsjs/[name].[ext]',
+    }]),
+  ],
 };
